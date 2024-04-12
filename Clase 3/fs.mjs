c9f0ca79af88154146e417d4d2086c6c5f8aa70a
Romaa83 <agustin_roma@outlook.com>
2024-04-12 12:28:35 -0300

    cosas

--- Clase 3/fs.mjs
+++ Clase 3/fs.mjs
@@ -1,7 +1,13 @@
 import { readFile } from "node:fs";
 import { join } from "node:path";
+import { mkdir } from "node:fs"; 
 
-readFile('si.txt', (err,data) => {
-    if (err) throw err
-    console.log(data.toString())
-})
+// readFile('si.txt', (err,data) => {
+//     if (err) throw err
+//     console.log(data.toString())
+// })
+
+const ruta2 = join('carpeta1', "carpeta2")
+mkdir(ruta2,{recursive: true}, (err) => {
+    if (err) throw err;
+  }); 
