fec1d374db889c75b1fde7881fa1041b16b067ea
Romaa83 <agustin_roma@outlook.com>
2024-04-13 13:28:04 -0300

    Crear Agregar Y Eliminar

--- Clase 3/fs.mjs
+++ Clase 3/fs.mjs
@@ -1,13 +1,36 @@
-import { readFile } from "node:fs";
+import { readFile, unlink } from "node:fs";
 import { join } from "node:path";
 import { mkdir } from "node:fs"; 
+import { writeFile } from "node:fs"
 
-// readFile('si.txt', (err,data) => {
-//     if (err) throw err
-//     console.log(data.toString())
-// })
+//Crear la Carpeta
 
-const ruta2 = join('carpeta1', "carpeta2")
-mkdir(ruta2,{recursive: true}, (err) => {
-    if (err) throw err;
-  }); 
+// const ruta2 = join('./Clase 3/carpeta1')
+// mkdir(ruta2,{recursive: true}, (err) => {
+//     if (err) throw err;
+//   }); 
+
+
+//Crear el archivo
+
+// readFile('./Clase 3/personas.json', (err, data) => {
+//   if (err) throw err;
+
+//   const datos = data.toString();
+//   const rutaArchivo = join('./Clase 3/carpeta1/archivo.txt')
+
+//     writeFile(rutaArchivo, datos, (err) => {
+//       if (err) throw err;
+//       console.log('El archivo se ha creado correctamente.');
+//   });
+// });
+
+// //Eliminar el archivo
+
+const rutaArchivo = join('./Clase 3/carpeta1/archivo.txt')
+unlink (rutaArchivo, (err) =>{
+  if (err) throw err
+  console.log("el archivo se elimino")
+})
+
+  
