3f3a4f9d372ad7019541ab126f146447e5596af4
Romaa83 <agustin_roma@outlook.com>
2024-04-15 12:25:28 -0300

    si

--- Clase 4 - HTTP/index.mjs
+++ Clase 4 - HTTP/index.mjs
@@ -4,12 +4,19 @@ const Servidor = http.createServer((peticion,respuesta)=>{
     //console.log(peticion);
     const ruta = peticion.url;
     const metodo = peticion.method
-    if (ruta === "/") {
-    respuesta.end(`La ruta es: ${ruta}` +`metodo ${metodo}` );
+    if (metodo === 'GET' && ruta === "/") {
+        respuesta.end('<h1>Si<h1>')
+        return;
     }
-    else if (ruta === "/saludo") {
-        respuesta.end(`Saludos ${ruta}`)
+    else {
+        respuesta.end('No encontrada')
     }
-    else {respuesta.end(`error ${ruta}`)}
+    // if (ruta === "/") {
+    // respuesta.end(`La ruta es: ${ruta}` +`metodo ${metodo}` );
+    // }
+    // else if (ruta === "/saludo") {
+    //     respuesta.end(`Saludos ${ruta}`)
+    // }
+    // else {respuesta.end(`error ${ruta}`)}
 });
 Servidor.listen(3000)
