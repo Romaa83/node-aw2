faa5c0d9ce5ef4c829450cd75e088cc55c4a7565
Romaa83 <agustin_roma@outlook.com>
2024-04-25 13:52:11 -0300

    Si

--- Clase 7/servidor.mjs
+++ Clase 7/servidor.mjs
@@ -1,48 +1,43 @@
 import http from "node:http"
 import fsp from "node:fs/promises"
 import path from "node:path"
-import fs from "node:fs"
-
-const ruta = path.join("publica" , "index.html")
-// const imagen = path.join("publica", "assets", "img", "pexels-pixabay-462023.jpg")
 
 async function gesIndex(peticion, respuesta){
     try{
         const archivo = await fsp.readFile(path.join("publica", "index.html"))
+        respuesta.statusCode = 200
         respuesta.end(archivo)
     }
     catch (err){
+        respuesta.statusCode = 404
         respuesta.end(err)
     }
-
 }
 
 async function gesRecursos(peticion, respuesta){
     try{
         
-        const rutaRecurso = path.join("publica", peticion.url)
-        const archivo = await fsp.readFile(rutaRecurso)
+        //const rutaRecurso = path.join("publica", peticion.url)
+        const archivo = await fsp.readFile(path.join("publica", peticion.url))
+        respuesta.statusCode = 200
+        respuesta.setHeader('Content-Type','image/jpeg')
         respuesta.end(archivo)
     }
     catch (err){
+        respuesta.statusCode = 404
         respuesta.end(err)
     }
 }
 
-
-
 const Servidor = http.createServer((peticion,respuesta)=>{
     if (peticion.method === "GET") {
-        if (peticion.url === "/") {
+        if (peticion.url === "/" || peticion.url === "/index.html") {
             gesIndex(peticion, respuesta)
          }
          else{
             gesRecursos(peticion, respuesta)
          }
-         return;
-    }
-    else{
-        
+          //return;
     }
 })
 Servidor.listen(3000)
