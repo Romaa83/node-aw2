ee66ac683f777c5b29932670f4f8516d1db65808
Romaa83 <agustin_roma@outlook.com>
2024-04-25 13:59:39 -0300

    lloro

--- Clase 7/servidor.mjs
+++ Clase 7/servidor.mjs
@@ -4,13 +4,13 @@ import path from "node:path"
 
 async function gesIndex(peticion, respuesta){
     try{
-        const archivo = await fsp.readFile(path.join("publica", "index.html"))
+        const archivo = await fsp.readFile(path.join('publica', 'index.html'))
         respuesta.statusCode = 200
         respuesta.end(archivo)
     }
     catch (err){
         respuesta.statusCode = 404
-        respuesta.end(err)
+        respuesta.end('ASDADS',err)
     }
 }
 
@@ -18,20 +18,20 @@ async function gesRecursos(peticion, respuesta){
     try{
         
         //const rutaRecurso = path.join("publica", peticion.url)
-        const archivo = await fsp.readFile(path.join("publica", peticion.url))
+        const archivo = await fsp.readFile(path.join('publica', peticion.url))
         respuesta.statusCode = 200
-        respuesta.setHeader('Content-Type','image/jpeg')
+        //respuesta.setHeader('Content-Type','image/jpeg')
         respuesta.end(archivo)
     }
     catch (err){
         respuesta.statusCode = 404
-        respuesta.end(err)
+        respuesta.end('eRROR',err)
     }
 }
 
 const Servidor = http.createServer((peticion,respuesta)=>{
     if (peticion.method === "GET") {
-        if (peticion.url === "/" || peticion.url === "/index.html") {
+        if (peticion.url === '/' || peticion.url === '/index.html') {
             gesIndex(peticion, respuesta)
          }
          else{
