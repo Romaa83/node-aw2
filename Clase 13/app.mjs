1677fb7d10255da5f9fcaa9115316a43c3640129
Romaa83 <agustin_roma@outlook.com>
2024-06-07 12:15:59 -0300

    sql

--- Clase 13/app.mjs
+++ Clase 13/app.mjs
@@ -6,6 +6,7 @@ import pg from 'pg'
 const {Pool} = pg; 
 const pool = new Pool({
     host: 'localhost',
+    port: 5432,
     user: 'root',
     password: 'pass',
     database: 'tienda',
@@ -23,7 +24,24 @@ app.listen(PUERTO, ()=>{
 })
 
 app.get("/productos", async (req, res)=>{
-    // const respuesta = await pool.query('select * from productos')
-    // console.log(respuesta)
-    res.send('postgres')
+    const respuesta = await pool.query('select * from productos')
+    console.log(respuesta.rows)
+    res.json(respuesta.rows)
+})
+
+app.get("/productos/:id", async (req, res)=>{
+    const {id} = req.params
+    const respuesta = await pool.query(`select * from productos where id=${id}`)
+    res.json(respuesta.rows)
+})
+
+app.post("/productos", async (req, res)=>{
+    const respuesta = await pool.query("INSERT INTO productos (producto, precio)" + " VALUES ($1,$2)",["hola",200])
+    res.json(respuesta.rows)
+})
+
+app.delete("/productos/:id", async(req, res)=>{
+    const {id} = req.params
+    const respuesta = await pool.query(`DELETE FROM productos WHERE id=${id}`)
+    res.json(respuesta.rows)
 })
